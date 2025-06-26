16785a6d011f5d06babf6df8849b3df7d01ca6c6
hayanmini <dudals9696@gmail.com>
2025-06-26 20:09:22 +0900

    기능 구현
    - 장바구니 담기 기능 구현
    - 장바구니 가격 출력 구현

--- bin/shopping_mall.dart
+++ bin/shopping_mall.dart
@@ -1,32 +1,48 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shopping_mall/shopping_mall.dart';
 
 void main(List<String> arguments) {
-  // print('Hello world: ${shopping_mall.calculate()}!');
-  //print(shopping_mall.Product);
-
-  stdout.write(
-      "[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료"); // run 출력값
-
-  var shoppingMall = ShoppingMall();
+  var shoppingMall = ShoppingMall(); // 변수 선언
 
   while (true) {
+    print(
+        '-------------------------------------------------------------------------------------------------------------');
+    print(
+        '[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료');
+    print(
+        '-------------------------------------------------------------------------------------------------------------');
+
+    // run 출력값
+
     String? numberInput = stdin.readLineSync();
-    //int number = int.parse(numberInput!);
     if (numberInput == "1") {
       shoppingMall.showProducts();
+      //상품 목록
     } else if (numberInput == "2") {
-      print("상품 이름과 상품 개수를 입력해주세요.");
+      print('상품 이름을 입력해 주세요 !\n');
+
+      String? productname =
+          stdin.readLineSync(encoding: Encoding.getByName('utf-8')!)?.trim();
+
+      print('상품 개수을 입력해 주세요 !\n');
+      String? productnumberInput = stdin.readLineSync()?.trim();
+      int productnumber = int.parse(productnumberInput!);
+      // 입력값 변환, 공백 제거
+      shoppingMall.addToCart(productname!, productnumber);
+      // 상품명, 상품 개수
     } else if (numberInput == "3") {
-      print("장바구니에 $ShoppingMall.totalPrice원 어치를 담으셨네요 !");
+      shoppingMall.showTotal();
+      // 총 가격 출력
     } else if (numberInput == "4") {
       print("이용해 주셔서 감사합니다 ~ 안녕히 가세요 !");
       break;
+      // 프로그램 종료
     } else {
       print("지원하지 않는 기능입니다 ! 다시 시도해 주세요 ..");
-    } // 입력값 대응
-  }
+    } // 1234외 입력 시
+  } // 입력값 대응
 
   /*
   try {
--- lib/shopping_mall.dart
+++ lib/shopping_mall.dart
@@ -8,24 +8,39 @@ class ShoppingMall {
     products.add(Product("반팔티", 35000));
     products.add(Product("반바지", 38000));
     products.add(Product("양말", 5000));
-  }
+  } // 상품
 
   void showProducts() {
     for (var p in products) {
-      print("${p.name} / ${p.price}");
+      print("${p.name} / ${p.price}원");
     }
   } // 상품 목록
 
-  void addToCart() {
-    // for (var i = 0; i < products.length; i++) {
-    //   var pd = products[i];
-    //   products[i].name , products[i].price
-    // }
-  }
+  void addToCart(String productname, int productnumber) {
+    // for (var pd in products) {
+    if (productnumber <= 0) {
+      print("0개보다 많은 개수의 상품만 담을 수 있어요 !");
+      return;
+    }
+
+    for (var i = 0; i < products.length; i++) {
+      var pd = products[i];
+
+      print("👉 디버깅: 비교 대상 상품명 = '${pd.name}'");
+      print("👉 디버깅: 입력한 상품명 = '${productname}'");
+      if (productname == pd.name) {
+        totalPrice += productnumber * pd.price;
+        print("장바구니에 상품이 담겼어요 !");
+        return;
+      } // 상품 가격 누적
+    } // 상품 리스트에서 입력값 대조하기
+
+    print("입력값이 올바르지 않아요 !"); // 입력값 오류 처리
+  } // 장바구니 담기
 
   void showTotal() {
-    //
-  }
+    print("장바구니에 $totalPrice원 어치를 담으셨네요 !");
+  } // 총 가격 출력
 } // 쇼핑몰 클래스
 
 class Product {
